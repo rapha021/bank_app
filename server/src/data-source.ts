1b2c329d00b473da05190ab3fbff98250fe40fca
Raphael Gloria <rapha.gloria@gmail.com>
2022-11-15 06:00:54 -0300

    feat: docker running ok

--- server/src/data-source.ts
+++ server/src/data-source.ts
@@ -3,7 +3,7 @@ import "dotenv/config"
 
 export const AppDataSource = new DataSource({
   type: "postgres",
-  host: "localhost",
+  host: "db",
   port: 5432,
 
   username: process.env.POSTGRES_USER,
